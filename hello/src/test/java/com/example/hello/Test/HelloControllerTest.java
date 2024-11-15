48c9791cf0e5510d77b7239960bc96105114f87d
Imdo714 <gusehdla@naver.com>
2024-11-15 21:19:17 +0900

    TEST3

--- hello/src/test/java/com/example/hello/Test/HelloControllerTest.java
+++ hello/src/test/java/com/example/hello/Test/HelloControllerTest.java
@@ -1,15 +1,15 @@
-package com.example.hello.Test;
-
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-public class HelloControllerTest {
-
-    @Test
-    void testSayHello() {
-        HelloController controller = new HelloController();
-        String response = controller.sayHello();
-        assertThat(response).isEqualTo("Hello, GitHub Actions!");
-    }
-}
+//package com.example.hello.Test;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//public class HelloControllerTest {
+//
+//    @Test
+//    void testSayHello() {
+//        HelloController controller = new HelloController();
+//        String response = controller.sayHello();
+//        assertThat(response).isEqualTo("Hello, GitHub Actions!");
+//    }
+//}
